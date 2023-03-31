eb1f0cfbcd4223e55709da190e6905a547ac99ce
Diyouf <diyoufkv7@gmail.com>
2023-03-31 16:32:11 +0530

    mongodb uploaded to Atlas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
 mongoose.set("strictQuery",false);
-mongoose.connect(process.env.MONGODB_CONNECTION)
+mongoose.connect(process.env.MONGODB_ATLAS_CONNECTION)
 
 const express = require("express");
 const app = express();
